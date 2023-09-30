2fb44587bb0f32e42876ef8b200c20a9dc743ccd
jincheol5 <oscar4293@gmail.com>
2023-09-30 22:21:04 +0900

    230930 22:20 update

---
+++ toMongoDB/dataLoad/src/main/java/sejong/dfpl/dataLoad/App.java
@@ -0,0 +1,13 @@
+package sejong.dfpl.dataLoad;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
